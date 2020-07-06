3ab76bcc7aea12d57ec03df38331d46e0a3f98a8
Anastasia <slyadneva.anastasia@gmail.com>
2020-07-06 14:28:33 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+// this line runs and connects to db
+require("./db/mongoose");
+const User = require("./models/user");
+const Task = require("./models/task");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// parses incomeing json data into an object accessible on req.body
+app.use(express.json());
+
+app.post("/users", (req, res) => {
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(() => {
+      res.status(201).send(user);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+app.listen(port, () => console.log(`Server listening on Port ${port}`));
