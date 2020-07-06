9817a7311c0f41bc7b54ea466131995e9a488d37
Anastasia <slyadneva.anastasia@gmail.com>
2020-07-06 15:21:40 -0500

    Created GET and POST endpoints for users and tasks

--- src/index.js
+++ src/index.js
@@ -23,6 +23,34 @@ app.post("/users", (req, res) => {
     });
 });
 
+app.get("/users", (req, res) => {
+  // fetches ALL users stored in the db
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  // req.params is an object containing all route parameters that were provided in the request
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+
+      res.send(user);
+    })
+    .catch(() => {
+      res.status(500).send();
+    });
+});
+
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
 
@@ -32,7 +60,35 @@ app.post("/tasks", (req, res) => {
       res.status(201).send(task);
     })
     .catch((err) => {
-      res.status(400).send(err);
+      res.status(400).send();
+    });
+});
+
+app.get("/tasks", (req, res) => {
+  // fetches ALL tasks stored in the db
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.get("/tasks/:id", (req, res) => {
+  // req.params is an object containing all route parameters that were provided in the request
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+
+      res.send(task);
+    })
+    .catch(() => {
+      res.status(500).send();
     });
 });
 
