f28cf368b434a2e122d12f3dcb36d77c104b0784
Anastasia <slyadneva.anastasia@gmail.com>
2020-07-07 13:47:15 -0500

    Refactor GET and POST endpoints to  async/await

--- src/index.js
+++ src/index.js
@@ -10,86 +10,89 @@ const port = process.env.PORT || 3000;
 // parses incomeing json data into an object accessible on req.body
 app.use(express.json());
 
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
 
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+  // Promise syntax
+  // user
+  //   .save()
+  //   .then(() => {
+  //     res.status(201).send(user);
+  //   })
+  //   .catch((err) => {
+  //     res.status(400).send(err);
+  //   });
+
+  // Async/await syntax
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
-app.get("/users", (req, res) => {
-  // fetches ALL users stored in the db
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
+app.get("/users", async (req, res) => {
+  try {
+    // fetches ALL users stored in the db
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
   // req.params is an object containing all route parameters that were provided in the request
   const _id = req.params.id;
 
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
+  try {
+    const user = await User.findById(_id);
 
-      res.send(user);
-    })
-    .catch(() => {
-      res.status(500).send();
-    });
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (err) {
+    res.status(500).send();
+  }
 });
 
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
 
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch((err) => {
-      res.status(400).send();
-    });
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (err) {
+    res.status(400).send();
+  }
 });
 
-app.get("/tasks", (req, res) => {
-  // fetches ALL tasks stored in the db
-  Task.find({})
-    .then((tasks) => {
-      res.send(tasks);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
+app.get("/tasks", async (req, res) => {
+  try {
+    // fetches ALL tasks stored in the db
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-app.get("/tasks/:id", (req, res) => {
+app.get("/tasks/:id", async (req, res) => {
   // req.params is an object containing all route parameters that were provided in the request
   const _id = req.params.id;
 
-  Task.findById(_id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).send();
-      }
-
-      res.send(task);
-    })
-    .catch(() => {
-      res.status(500).send();
-    });
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (err) {
+    res.status(500).send();
+  }
 });
 
 app.listen(port, () => console.log(`Server listening on Port ${port}`));
